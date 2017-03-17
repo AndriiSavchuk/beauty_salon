83c0b8dbf40ad9fa42d6a542a19301d67eb3e63e
Andrii <andriksr@gmail.com>
2017-03-17 17:44:16 +0200

    Initial commit

---
+++ app/js/scripts.js
@@ -0,0 +1,16 @@
+$(function() {
+
+    /* Auto resize elements */
+    //
+    // var hf = function(){
+    //     var headerHeight = $('header').height();
+    //     var footerHeight = $('footer').height();
+    //     $('.content').css({
+    //         'paddingTop': headerHeight,
+    //         'paddingBottom': footerHeight
+    //     });
+    // };
+    //
+    // $(window).bind('load resize', hf);
+
+});
